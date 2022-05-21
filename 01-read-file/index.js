c88db55dc0b1d99165df6da3a645b0f3e39bafba
Pavel <pavel.priladyshev@gmail.com>
2022-05-21 09:16:20 +0300

    feat: complete task 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,8 @@
-// start task
+const fs = require('fs');
+const path = require('path');
+
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString().trim());
+});
