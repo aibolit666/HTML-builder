4516be173743ab6bf6511de4c50869d3bf1e59ef
Pavel <pavel.priladyshev@gmail.com>
2022-05-21 09:53:28 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    files.forEach((file) => {
+      fs.stat(path.join(__dirname, 'files', file), (err, stats) => {
+        if (err) {
+          throw err;
+        }
+        if (stats.isFile() === true) {
+          fs.copyFile(
+            path.join(__dirname, 'files', file),
+            path.join(__dirname, 'files-copy', file),
+            (err) => {
+              if (err) {
+                throw err;
+              }
+            }
+          );
+        }
+      });
+    });
+    if (err) {
+      throw err;
+    }
+    console.log(
+      `Success!\nFiles (${files})\ncopied to ${__dirname + '\\files-copy'}`
+    );
+  });
+}
+
+copyDir();
